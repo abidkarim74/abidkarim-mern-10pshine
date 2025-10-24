def5d7890dfd719719537433b5c47ea470316ffe
abidkarim <abidkarim737@gmail.com>
2025-10-24 11:07:25 +0500

    Tested the api using postman

--- backend/src/routes/notes_routes.ts
+++ backend/src/routes/notes_routes.ts
@@ -9,8 +9,8 @@ const notes_router = Router();
 notes_router.get('/auth-notes', verify_authentication, auth_notes_list);
 notes_router.get('/general-notes', verify_authentication, general_notes_list);
 notes_router.post('/create-note', verify_authentication, create_note);
-notes_router.put('/update-note', verify_authentication, update_note);
-notes_router.delete('/delete-note', verify_authentication, delete_note);
+notes_router.put('/update-note/:id', verify_authentication, update_note);
+notes_router.delete('/delete-note/:id', verify_authentication, delete_note);
 
 
 export default notes_router;
